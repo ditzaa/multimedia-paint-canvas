82e5e648f853ceee4e6a422b850fac2511362f35
Mitu Adrian <adrianmitu83@gmail.com>
2023-12-22 12:09:26 +0200

    Added boilerplate for canvas element

---
+++ main.js
@@ -0,0 +1,18 @@
+/**
+ * @type {HTMLCanvasElement} canvas - Elementul canvas din DOM
+ * @type {CanvasRenderingContext2D} ctx - Contextul 2D al canvasului
+ */
+const canvas = document.getElementById("canvas");
+canvas.width = 800;
+canvas.height = 550;
+const ctx = canvas.getContext("2d");
+
+
+
+
+
+
+
+
+
+
