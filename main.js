1a20eb4c2330cc8e37244af2cb9a0b70a3e71326
Mitu Adrian <adrianmitu83@gmail.com>
2023-12-23 13:40:20 +0200

    Added size of line modifier as slider

--- main.js
+++ main.js
@@ -6,59 +6,59 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const btnRect = document.getElementById('btn-dreptunghi');
 const btnElipsa = document.getElementById('btn-elipsa');
+const btnLine = document.getElementById('btn-linie');
+const btnSaveRaster = document.getElementById('btn-save-raster');
+const btnSaveVectorial = document.getElementById('btn-save-svg');
+const inputLineSize = document.getElementById('input-line-size');
 
 //set canvas dimensions
 canvas.width = 800;
 canvas.height = 500;
-
-canvas.addEventListener("mousedown", startDrawing);
-canvas.addEventListener("mousemove", whileDrawing);
-canvas.addEventListener("mouseup", stopDrawing);
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 let tool = '',
 isPainting = false,
 prevMouseX, 
 prevMouseY,
-beforeDrawing;
+beforeDrawing,
+lineSize = 10;
+
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mousemove", whileDrawing);
+canvas.addEventListener("mouseup", stopDrawing);
 
 btnRect.addEventListener('click', () =>{
     // console.log("Am ales sa desenez un dreptunghi");
     tool = 'rectangle';
-})
+});
 
 btnElipsa.addEventListener('click', () =>{
-    console.log("Am ales sa desenez o elipsa");
     tool = 'elipsa';
+});
+
+btnLine.addEventListener('click', () =>{
+    console.log("Am ales sa desenez o linie");
+    tool = 'line';
+});
+
+btnSaveRaster.addEventListener('click', () =>{
+    const dataUrl = canvas.toDataURL('/media/png', 1);
+    const a = document.createElement("a");
+    a.href = dataUrl;
+    a.download = 'chart.png';
+    a.click();
+});
+
+
+btnSaveVectorial.addEventListener('click', () =>{
+});
+
+//"change", () => brushWidth = sizeSlider.value
+inputLineSize.addEventListener('change', ()=>{
+    lineSize = inputLineSize.value;
+    console.log(lineSize);
 })
-
-// function drawElipsaWeird(e){
-//     // console.log("OFFSET X: " + e.offsetX);
-//     // console.log("OFFSET Y: " + e.offsetY);
-//     // console.log("PREV X: " + prevMouseX);
-//     // console.log("PREV Y: " + prevMouseY);
-//     ctx.beginPath();
-//     let startPointX = e.offsetX;
-//     let startPointY = (e.offsetY - (e.offsetY - prevMouseY)/2);
-//     ctx.moveTo(startPointX, startPointY);
-//     // console.log(startPointX, startPointY);
-//     let controlPointX =  e.offsetX - (e.offsetX - prevMouseX)/2;
-//     let controlPointY = e.offsetY;
-//     // console.log(controlPointX, controlPointY);
-//     let endPointX = prevMouseX;
-//     let endPointY = (e.offsetY - (e.offsetY - prevMouseY)/2);
-//     ctx.quadraticCurveTo(controlPointX,
-//         controlPointY, endPointX, endPointY);
-//     ctx.stroke();
-    
-//     ctx.beginPath();
-//     controlPointX = e.offsetX - (e.offsetX - prevMouseX)/2;
-//     controlPointY = prevMouseY;
-//     ctx.moveTo(endPointX, endPointY);
-//     ctx.quadraticCurveTo(controlPointX,
-//         controlPointY, startPointX, startPointY);
-//     ctx.stroke();
-
-// }  
 
 function drawElipsa(e){
     ctx.beginPath();
@@ -66,7 +66,6 @@ function drawElipsa(e){
     let centerEllipseY = (e.offsetY - (e.offsetY - prevMouseY)/2);
     let radiusX = Math.abs((e.offsetX - prevMouseX)/2);
     let radiusY =  Math.abs((e.offsetY - prevMouseY)/2);
-    console.log(radiusX, radiusY);
     //ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle)
     ctx.ellipse(centerEllipseX, centerEllipseY, radiusX, radiusY,
         0, 0, 2 * Math.PI);
@@ -81,11 +80,19 @@ function drawRectangle(e){
     // console.log("PREV Y: " + prevMouseY);
 }
 
+function drawLine(e){
+    ctx.beginPath();
+    ctx.moveTo(prevMouseX, prevMouseY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+}
+
 function startDrawing(e){
     isPainting = true;
     prevMouseX = e.offsetX;
     prevMouseY = e.offsetY; 
     ctx.beginPath();
+    ctx.lineWidth = lineSize;
     beforeDrawing = ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
 
@@ -96,12 +103,16 @@ function whileDrawing(e){
         drawRectangle(e);
     }else if(tool === 'elipsa'){
         drawElipsa(e);
+    }else if(tool == "line"){
+        drawLine(e);
     }
 }
 
 function stopDrawing(){
     isPainting = false;
 }
+
+
 
 
 
